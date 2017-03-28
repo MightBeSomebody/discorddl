36d0f67c87e94a8edc40778a15c43fd5829fb4d6
MightBeSomebody <Adam24Williams@gmail.com>
2017-03-28 12:59:45 -0400

    Update mainBot.php

--- mainBot.php
+++ mainBot.php
@@ -6,7 +6,7 @@ include('./httpful.phar');
 include('./functions.php');
 
 // this is found at http://dev.groupme.com/bots
-$bot_token = "the main bot id";
+$bot_token = "296292973640155148";
 
 include('./index.php');
 
